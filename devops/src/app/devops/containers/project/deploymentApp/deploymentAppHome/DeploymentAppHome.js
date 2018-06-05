31660bf5eef199a50f849a9e9b0e4fc74d5dfde8
huihuawk <huihua_wk@163.com>
2018-06-05 10:39:25 +0800

    [IMP]imp app deploy page style

--- devops/src/app/devops/containers/project/deploymentApp/deploymentAppHome/DeploymentAppHome.js
+++ devops/src/app/devops/containers/project/deploymentApp/deploymentAppHome/DeploymentAppHome.js
@@ -1,20 +1,19 @@
 import React, { Component } from 'react';
 import { observer, inject } from 'mobx-react';
 import { withRouter } from 'react-router-dom';
-import { Select, Button, Spin, Radio, Card, Steps } from 'choerodon-ui';
+import { Select, Button, Spin, Radio, Steps } from 'choerodon-ui';
 import _ from 'lodash';
 import PageHeader from 'PageHeader';
 import Permission from 'PerComponent';
 import '../../../main.scss';
 import './DeployApp.scss';
-import MouserOverWrapper from '../../../../components/MouseOverWrapper';
 import AceForYaml from '../../../../components/yamlAce';
 import SelectApp from '../selectApp';
 
 
 const RadioGroup = Radio.Group;
-const Option = Select.Option;
 const Step = Steps.Step;
+
 @inject('AppState')
 @observer
 class DeploymentAppHome extends Component {
@@ -32,7 +31,7 @@ class DeploymentAppHome extends Component {
   }
 
   componentDidMount() {
-    const { AppState, DeploymentAppStore } = this.props;
+    const { DeploymentAppStore } = this.props;
     if (this.state.appId) {
       DeploymentAppStore.loadApps(this.state.appId)
         .then((data) => {
@@ -50,7 +49,6 @@ class DeploymentAppHome extends Component {
    */
   getStatus = (index) => {
     const { current } = this.state;
-    const { appId, envId, versionId, mode, instanceId } = this.state;
     let status = 'process';
     if (index === current) {
       status = 'process';
@@ -60,33 +58,37 @@ class DeploymentAppHome extends Component {
       status = 'finish';
     }
     return status;
-  }
+  };
+
   /**
    * 改变步骤条
    * @param index
    */
-  changeStep =(index) => {
+  changeStep = (index) => {
     this.setState({ current: index });
-  }
+  };
+
   /**
    * 展开选择应用的弹框
    */
-  showSideBar =() => {
+  showSideBar = () => {
     this.setState({ show: true });
-  }
+  };
+
   /**
    * 关闭弹框
    */
   handleCancel = () => {
     this.setState({ show: false });
-  }
+  };
+
   /**
    * 弹框确定
    * @param app 选择的数据
    * @param key 标明是项目应用还是应用市场应用
    */
   handleOk = (app, key) => {
-    const { AppState, DeploymentAppStore } = this.props;
+    const { DeploymentAppStore } = this.props;
     if (key === '1') {
       DeploymentAppStore.loadVersion(app.id);
       this.setState({ app, appId: app.id, show: false });
@@ -94,13 +96,14 @@ class DeploymentAppHome extends Component {
       DeploymentAppStore.loadVersion(app.appId);
       this.setState({ app, appId: app.appId, show: false });
     }
-  }
+  };
+
   /**
    * 选择环境
    * @param value
    */
-  handleSelectEnv =(value) => {
-    const { AppState, DeploymentAppStore } = this.props;
+  handleSelectEnv = (value) => {
+    const { DeploymentAppStore } = this.props;
     const envs = DeploymentAppStore.envs;
     const envDto = _.filter(envs, v => v.id === value)[0];
     this.setState({ envId: value, envDto, value: null });
@@ -108,43 +111,47 @@ class DeploymentAppHome extends Component {
     DeploymentAppStore.setValue(null);
     DeploymentAppStore.loadValue(appId, versionId, value);
     DeploymentAppStore.loadInstances(this.state.appId, value);
-  }
+  };
+
   /**
    * 选择版本
    * @param value
    */
-  handleSelectVersion =(value) => {
-    const { AppState, DeploymentAppStore } = this.props;
+  handleSelectVersion = (value) => {
+    const { DeploymentAppStore } = this.props;
     this.setState({ versionId: value });
     const versions = DeploymentAppStore.versions;
     const versionDto = _.filter(versions, v => v.id === value)[0];
     this.setState({ versionDto });
-  }
+  };
+
   /**
    * 选择实例
    * @param value
    */
-  handleSelectInstance =(value) => {
-    const { AppState, DeploymentAppStore } = this.props;
+  handleSelectInstance = (value) => {
+    const { DeploymentAppStore } = this.props;
     const instance = DeploymentAppStore.currentInstance;
     const instanceDto = _.filter(instance, v => v.id === value)[0];
     this.setState({ instanceId: value, instanceDto });
-  }
+  };
+
   /**
    * 获取values
    * @param value
+   * @param markers
    */
-  handleChangeValue =(value, markers) => {
+  handleChangeValue = (value, markers) => {
     this.setState({ value, markers });
-  }
+  };
+
   /**
    * 修改实例模式
    * @param value
    */
-  handleChangeMode =(value) => {
-    const { AppState, DeploymentAppStore } = this.props;
+  handleChangeMode = (value) => {
     this.setState({ mode: value.target.value });
-  }
+  };
 
   /**
    * 返回到上一级
@@ -163,7 +170,7 @@ class DeploymentAppHome extends Component {
    * 取消返回到第一步
    */
   clearAll = () => {
-    const { AppState, DeploymentAppStore } = this.props;
+    const { DeploymentAppStore } = this.props;
     DeploymentAppStore.setVersions([]);
     DeploymentAppStore.setValue(null);
     DeploymentAppStore.setCurrentInstance([]);
@@ -178,12 +185,13 @@ class DeploymentAppHome extends Component {
       instanceId: undefined,
       current: 1,
     });
-  }
+  };
+
   /**
    * 取消第一步
    */
   clearStepOne = () => {
-    const { AppState, DeploymentAppStore } = this.props;
+    const { DeploymentAppStore } = this.props;
     DeploymentAppStore.setVersions([]);
     this.setState({
       appId: undefined,
@@ -191,12 +199,13 @@ class DeploymentAppHome extends Component {
       versionId: undefined,
       versionDto: null,
     });
-  }
+  };
+
   /**
    * 部署应用
    */
   handleDeploy = () => {
-    const { AppState, DeploymentAppStore } = this.props;
+    const { DeploymentAppStore } = this.props;
     const value = this.state.value || DeploymentAppStore.value.yaml;
     const applicationDeployDTO = {
       appId: this.state.appId || DeploymentAppStore.currentApp.id,
@@ -210,7 +219,6 @@ class DeploymentAppHome extends Component {
     DeploymentAppStore.deploymentApp(applicationDeployDTO)
       .then((datas) => {
         if (datas) {
-          // Choerodon.prompt('创建成功');
           this.openAppDeployment();
         }
       })
@@ -221,7 +229,7 @@ class DeploymentAppHome extends Component {
   /**
    * 渲染第一步
    */
-  handleRenderApp =() => {
+  handleRenderApp = () => {
     const { DeploymentAppStore } = this.props;
     const versions = DeploymentAppStore.versions;
     return (
@@ -235,10 +243,18 @@ class DeploymentAppHome extends Component {
             <span className="section-title">选择应用</span>
           </div>
           <div className="deploy-text">
-            { this.state.app && <div className="section-text-margin"> <span className="icon-project section-text-icon" />
-              <span className="section-text≈">{this.state.app && this.state.app.name}({this.state.app && this.state.app.code})</span>
-            </div> }
-            <a role="none" className={`${this.state.app ? '' : 'section-text-margin'}`} onClick={this.showSideBar}>打开应用列表 <span className="icon-open_in_new" /></a>
+            {this.state.app && <div className="section-text-margin">
+              {this.state.app.publishLevel ? <span className="icon-apps section-text-icon" /> : <span className="icon-project section-text-icon" />}
+              <span className="section-text">{this.state.app.name}({this.state.app.code})</span>
+            </div>}
+            <a
+              role="none"
+              className={`${this.state.app ? '' : 'section-text-margin'}`}
+              onClick={this.showSideBar}
+            >
+              打开应用列表
+              <span className="icon-open_in_new" />
+            </a>
           </div>
         </section>
         <section className="deployApp-section">
@@ -247,11 +263,11 @@ class DeploymentAppHome extends Component {
             <span className="section-title">选择版本</span>
           </div>
           <Select
-            value={this.state.versionId ? parseInt(this.state.versionId, 10) : ''}
-            label={<span className="deploy-text">应用版本</span>}
+            value={this.state.versionId ? parseInt(this.state.versionId, 10) : undefined}
+            label="应用版本"
             className="section-text-margin"
             onSelect={this.handleSelectVersion}
-            style={{ width: 512 }}
+            style={{ width: 482 }}
             optionFilterProp="children"
             filterOption={(input, option) => option.props.children
               .toLowerCase().indexOf(input.toLowerCase()) >= 0}
@@ -261,17 +277,25 @@ class DeploymentAppHome extends Component {
           </Select>
         </section>
         <section className="deployApp-section">
-          <Button type="primary" funcType="raised" disabled={!(this.state.appId && this.state.versionId)} onClick={this.changeStep.bind(this, 2)}>下一步</Button>
+          <Button
+            type="primary"
+            funcType="raised"
+            disabled={!(this.state.appId && this.state.versionId)}
+            onClick={this.changeStep.bind(this, 2)}
+          >
+            下一步
+          </Button>
           <Button funcType="raised" onClick={this.clearStepOne}>取消</Button>
         </section>
       </div>
     );
-  }
+  };
+
   /**
    * 渲染第二步
    */
   handleRenderEnv = () => {
-    const { AppState, DeploymentAppStore } = this.props;
+    const { DeploymentAppStore } = this.props;
     const envs = DeploymentAppStore.envs;
     const data = DeploymentAppStore.value;
     return (
@@ -289,14 +313,14 @@ class DeploymentAppHome extends Component {
             label={<span className="deploy-text">环境</span>}
             className="section-text-margin"
             onSelect={this.handleSelectEnv}
-            style={{ width: 512 }}
+            style={{ width: 482 }}
             optionFilterProp="children"
             filterOption={(input, option) => option.props.children[1]
               .toLowerCase().indexOf(input.toLowerCase()) >= 0}
             filter
           >
             {envs.map(v => (<option value={v.id}>
-              {v.connect ? <span className="icon-link env-icon-success" /> : <span className="icon-baseLine-link_off env-icon-error" />}
+              {v.connect ? <span className="c7n-ist-status_on" /> : <span className="c7n-ist-status_off" />}
               {v.name}
             </option>))}
           </Select>
@@ -306,17 +330,22 @@ class DeploymentAppHome extends Component {
             <span className="icon-description section-title-icon " />
             <span className="section-title">配置信息</span>
           </div>
-          {data && <div className="deploy-text">
-            {<AceForYaml
-              modifyMarkers={this.state.markers}
-              value={this.state.value || data.yaml}
-              highlightMarkers={data.highlightMarkers}
-              onChange={this.handleChangeValue}
-            />}
-          </div>}
+          {data && (<AceForYaml
+            modifyMarkers={this.state.markers}
+            value={this.state.value || data.yaml}
+            highlightMarkers={data.highlightMarkers}
+            onChange={this.handleChangeValue}
+          />)}
         </section>
         <section className="deployApp-section">
-          <Button type="primary" funcType="raised" onClick={this.changeStep.bind(this, 3)} disabled={!(this.state.envId && (this.state.value || (data && data.yaml)))}>下一步</Button>
+          <Button
+            type="primary"
+            funcType="raised"
+            onClick={this.changeStep.bind(this, 3)}
+            disabled={!(this.state.envId && (this.state.value || (data && data.yaml)))}
+          >
+            下一步
+          </Button>
           <Button onClick={this.changeStep.bind(this, 1)} funcType="raised">上一步</Button>
         </section>
       </div>
@@ -327,12 +356,7 @@ class DeploymentAppHome extends Component {
    * @returns {*}
    */
   handleRenderMode = () => {
-    const { AppState, DeploymentAppStore } = this.props;
-    const radioStyle = {
-      display: 'block',
-      height: '30px',
-      lineHeight: '30px',
-    };
+    const { DeploymentAppStore } = this.props;
     const instances = DeploymentAppStore.currentInstance;
     return (
       <div className="deployApp-deploy">
@@ -345,16 +369,21 @@ class DeploymentAppHome extends Component {
             <span className="section-title">选择部署模式</span>
           </div>
           <div className="section-text-margin">
-            <RadioGroup onChange={this.handleChangeMode} value={this.state.mode} label={<span className="deploy-text">部署模式</span>}>
-              <Radio style={radioStyle} value={'new'}>新建实例</Radio>
-              <Radio style={radioStyle} value={'replace'} disabled={instances.length === 0}>替换实例
+            <RadioGroup
+              onChange={this.handleChangeMode}
+              value={this.state.mode}
+              label={<span className="deploy-text">部署模式</span>}
+            >
+              <Radio className="deploy-radio" value={'new'}>新建实例</Radio>
+              <Radio className="deploy-radio" value={'replace'} disabled={instances.length === 0}>替换实例
                 <span className="icon-error section-instance-icon" />
-                <span style={{ color: 'rgba(0,0,0,0.65)' }}>替换实例会更新该实例的镜像及配置信息，请确认要替换的实例选择无误。</span></Radio>
+                <span className="deploy-tip-text">替换实例会更新该实例的镜像及配置信息，请确认要替换的实例选择无误。</span>
+              </Radio>
             </RadioGroup>
             {this.state.mode === 'replace' && <Select
               onSelect={this.handleSelectInstance}
               label="选择要替换的实例"
-              style={{ width: 300 }}
+              className="deploy-select"
               placeholder="Select a person"
               optionFilterProp="children"
               filterOption={(input, option) => option.props.children
@@ -364,16 +393,24 @@ class DeploymentAppHome extends Component {
               {instances.map(v => (<option value={v.id}>
                 {v.code}
               </option>))}
-            </Select> }
+            </Select>}
           </div>
         </section>
         <section className="deployApp-section">
-          <Button type="primary" funcType="raised" onClick={this.changeStep.bind(this, 4)} disabled={!this.state.mode}>下一步</Button>
+          <Button
+            type="primary"
+            funcType="raised"
+            onClick={this.changeStep.bind(this, 4)}
+            disabled={!this.state.mode}
+          >
+            下一步
+          </Button>
           <Button funcType="raised" onClick={this.changeStep.bind(this, 2)}>上一步</Button>
         </section>
       </div>
     );
-  }
+  };
+
   /**
    * 渲染第四步
    * @returns {*}
@@ -386,7 +423,9 @@ class DeploymentAppHome extends Component {
         <section>
           <div>
             <div className="deployApp-title"><span className="icon-widgets" />应用名称：</div>
-            <div className="deployApp-text">{this.state.app && this.state.app.name} <span className="deployApp-value">({this.state.app && this.state.app.code})</span></div>
+            <div className="deployApp-text">{this.state.app && this.state.app.name}
+              <span className="deployApp-value">({this.state.app && this.state.app.code})</span>
+            </div>
           </div>
           <div>
             <div className="deployApp-title"><span className="icon-version" />应用版本：</div>
@@ -394,11 +433,14 @@ class DeploymentAppHome extends Component {
           </div>
           <div>
             <div className="deployApp-title"><span className="icon-donut_large" />选择环境：</div>
-            <div className="deployApp-text">{this.state.envDto && this.state.envDto.name} <span className="deployApp-value">({this.state.envDto && this.state.envDto.code})</span></div>
+            <div className="deployApp-text">{this.state.envDto && this.state.envDto.name}
+              <span className="deployApp-value">({this.state.envDto && this.state.envDto.code})</span>
+            </div>
           </div>
           <div>
             <div className="deployApp-title"><span className="icon-jsfiddle" />部署模式：</div>
-            <div className="deployApp-text">{this.state.mode === 'new' ? '新建实例' : '替换实例'} {this.state.mode === 'replace' && <span className="deployApp-value">{this.state.instanceDto.code}</span> }</div>
+            <div className="deployApp-text">{this.state.mode === 'new' ? '新建实例' : '替换实例'} {this.state.mode === 'replace' &&
+            <span className="deployApp-value">{this.state.instanceDto.code}</span>}</div>
           </div>
           <div>
             <div className="deployApp-title"><span className="icon-description" />配置信息：</div>
@@ -420,7 +462,7 @@ class DeploymentAppHome extends Component {
         </section>
       </section>
     );
-  }
+  };
 
   render() {
     const { AppState, DeploymentAppStore } = this.props;
@@ -430,18 +472,23 @@ class DeploymentAppHome extends Component {
     return (
       <div className="c7n-region c7n-deployApp page-container">
         <PageHeader title={Choerodon.languageChange('deploymentApp.title')} />
-        <div className="page-content c7n-deployApp-wrapper" style={{ paddingBottom: '16px' }}>
+        <div className="page-content c7n-deployApp-wrapper">
           <h2 className="c7n-space-first">项目&quot;{projectName}&quot;的部署应用</h2>
           <p>
-            部署应用是一个将指定版本的应用部署至指定环境的操作。
-            <a href="http://choerodon.io/zh/docs/user-guide/deploy/application-deployment/" className="c7n-external-link">
+            应用部署是一个将某版本的应用部署至某环境的操作。您可以在此按指引分步骤完成应用部署。
+            <a
+              href="http://choerodon.io/zh/docs/user-guide/deploy/application-deployment/"
+              className="c7n-external-link"
+              rel="nofollow me noopener noreferrer"
+              target="_blank"
+            >
               <span className="c7n-external-link-content">
                 了解详情
               </span>
               <span className="icon-open_in_new" />
             </a>
           </p>
-          <Card className="deployApp-card">
+          <div className="deployApp-card">
             <Steps current={this.state.current}>
               <Step
                 title={<span style={{ color: current === 1 ? '#3F51B5' : '', fontSize: 14 }}>选择应用及版本</span>}
@@ -465,15 +512,15 @@ class DeploymentAppHome extends Component {
               />
             </Steps>
             <div className="deployApp-card-content">
-              { this.state.current === 1 && this.handleRenderApp()}
+              {this.state.current === 1 && this.handleRenderApp()}
 
-              { this.state.current === 2 && this.handleRenderEnv()}
+              {this.state.current === 2 && this.handleRenderEnv()}
 
-              { this.state.current === 3 && this.handleRenderMode() }
+              {this.state.current === 3 && this.handleRenderMode()}
 
-              { this.state.current === 4 && this.handleRenderReview() }
+              {this.state.current === 4 && this.handleRenderReview()}
             </div>
-          </Card>
+          </div>
           {this.state.show && <SelectApp
             show={this.state.show}
             handleCancel={this.handleCancel}
