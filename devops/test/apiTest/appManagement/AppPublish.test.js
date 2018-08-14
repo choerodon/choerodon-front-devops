5db6253a11fb896471f88c2cd04231073c5d26be
曹昱 <yu.cao04@hand-china.com>
2018-08-14 09:30:43 +0800

    [ADD] version.test and publish.test

---
+++ devops/test/apiTest/appManagement/AppPublish.test.js
@@ -0,0 +1,100 @@
+const appPublish = require("../../apiFunction/appManagement/AppPublish");
+const uuidv1 = require("uuid/v1");
+const { oauth, login } = require("../../Utils");
+const path = require("path");
+
+describe("AppPublish Api", function() {
+  it("[POST] 查询已启用但未发布且有版本的应用", function() {
+    const data = { searchParam: {}, param: "" };
+    const project = oauth.project;
+    return appPublish.getUnPublishList({ project, data });
+  });
+
+  it("[GET] 查询单个应用市场的应用详情", function() {
+    const project = oauth.project;
+    const data = {
+      id: 87,
+      appId: 322
+    };
+    return appPublish.getApplicationDetail({ project, data });
+  });
+
+  it("[PUT] 更新单个应用市场的应用", function() {
+    const project = oauth.project;
+    const data = {
+      contributor: "贡献1",
+      category: "分类1",
+      description: "描述1",
+      id: 87,
+      appId: 322,
+      publishLevel: "orgnization"
+    };
+    return appPublish.updateApplicationDetail({ project, data });
+  });
+
+  it("[PUT] 发布单个未发布的应用的所有版本", function() {
+    const project = oauth.project;
+    const appVersions = [
+      {
+        id: 312,
+        version: "2018.7.30-144557-master",
+        creationDate: "2018-07-30 14:56:32",
+        updatedDate: "2018-07-30 14:56:32",
+        publish: null,
+        deployed: false
+      }
+    ];
+    return appPublish.publishApplicationVersion({
+      project,
+      id: 87,
+      appVersions
+    });
+  });
+
+  it("[POST] 发布单个应用", function() {
+    const project = oauth.project;
+    const appVersions = [
+      {
+        id: 322,
+        version: "2018.8.13-171020-master",
+        commit: "57ec4c5327ca46b11baa331f1d39798bf1de4520",
+        appName: "813fronttest",
+        appCode: "baiyisantest",
+        appId: 368,
+        appStatus: true,
+        creationDate: "2018-08-13 17:19:19"
+      }
+    ];
+    const data = {
+      appId:368,
+      category:'分类1',
+      contributor:'贡献1',
+      description:'描述1',
+      imgUrl:'http://minio.staging.saas.hand-china.com/devops-service/file_60a53ee6f063477db9ff361d82fd6dd4_icon',
+      publishLevel:'orgnization'
+    }
+    return appPublish.publishApplication({project,appVersions,data});
+  });
+
+  it("[POST] 获取单个应用的已发布的版本", function() {
+    const project = oauth.project;
+    const id = 87;
+    return appPublish.getApplicationVersions({ project, id, is_publish: true });
+  });
+
+  it("[POST] 获取单个应用的未发布的版本", function() {
+    const project = oauth.project;
+    const id = 87;
+    return appPublish.getApplicationVersions({
+      project,
+      id,
+      is_publish: false
+    });
+  });
+
+  it("[POST] 上传单个应用的图标", function() {
+    const file_name = "icon.jpg";
+    const filePath = path.join(__dirname, file_name);
+    return appPublish.sendApplicationIcon({ filePath, file_name });
+  });
+});
