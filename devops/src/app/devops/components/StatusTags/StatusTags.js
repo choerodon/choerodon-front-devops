80062ed6d636502d64e62e2db4012faade82a51a
huihuawk <huihua_wk@163.com>
2018-09-18 12:34:20 +0800

    [ADD]add status tag components

---
+++ devops/src/app/devops/components/StatusTags/StatusTags.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import './StatusTags.scss';
+
+const Color = {
+  success: '#1bc123',
+  error: '#f44336',
+};
+
+class StatusTags extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return !(nextProps.name === this.props.name
+      && nextProps.color === this.props.color);
+  }
+
+  render() {
+    const { name, color, colorCode } = this.props;
+    return (
+      <div
+        className="c7n-status-tags"
+        style={{
+          background: color || Color[colorCode] || 'rgba(0, 0, 0, 0.28)',
+          ...this.props.style,
+        }}
+      >
+        <div>{ name || '' }</div>
+      </div>
+    );
+  }
+}
+export default StatusTags;
