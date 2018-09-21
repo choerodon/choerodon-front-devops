9893a9179948e527317d1f9beadb1f507e063baf
ale <ale0720@163.com>
2018-09-21 11:10:44 +0800

    [ADD]add a chart for commits

--- devops/src/app/devops/containers/project/reports/Submission/Submission.js
+++ devops/src/app/devops/containers/project/reports/Submission/Submission.js
@@ -2,125 +2,209 @@ import React, { Component } from 'react';
 import { observer } from 'mobx-react';
 import { injectIntl, FormattedMessage } from 'react-intl';
 import { Page, Header, Content, stores, Permission } from 'choerodon-front-boot';
-import { Select, Button, DatePicker } from 'choerodon-ui';
+import { Select, Button } from 'choerodon-ui';
+import _ from 'lodash';
 import ChartSwitch from '../Component/ChartSwitch';
 import LineChart from './LineChart';
 import CommitHistory from './CommitHistory';
+import TimePicker from '../Component/TimePicker';
 import './Submission.scss';
 import '../../../main.scss';
 
+/**
+ * 将数据转为图表可用格式
+ * @param data
+ * @returns {{total, user: Array}}
+ */
+function formatData(data) {
+  const { totalCommitsDate, commitFormUserDTOList } = data;
+  const total = {};
+  const user = [];
+  total.items = _.countBy(totalCommitsDate, item => item.slice(0, 10));
+  total.count = totalCommitsDate.length;
+  _.forEach(commitFormUserDTOList, (item) => {
+    const { name, imgUrl, commitDates } = item;
+    const userTotal = {
+      name,
+      avatar: imgUrl,
+    };
+    userTotal.items = _.countBy(commitDates, cit => cit.slice(0, 10));
+    userTotal.count = commitDates.length;
+    user.push(userTotal);
+  });
+
+  return {
+    total,
+    user,
+  };
+}
+
 const { AppState } = stores;
 const { Option } = Select;
-const { RangePicker } = DatePicker;
 
-const commitsData = [{
-  commitsId: 1,
-  commitContent: 'Update .gitlab-ci.yml',
-  commitDate: '2018-08-15 10:14:10',
-  commitUrl: 'http://git.staging.saas.hand-china.com/operation-pro0815/app0815/commit/c4cfa9d84e29984c3af3467dc0d800e75d5df268?view=parallel',
-  commitUserName: 'crockitwood2',
-  commitUserUrl: 'http://minio.staging.saas.hand-china.com/iam-service/file_d0c99fab16c14c38ac023bca0110ac05_WX20180720-141353%402x.png',
-}, {
-  commitsId: 2,
-  commitContent: 'Update .gitlab-ci.yml',
-  commitDate: '2018-08-15 10:14:10',
-  commitUrl: 'http://git.staging.saas.hand-china.com/operation-pro0815/app0815/commit/c4cfa9d84e29984c3af3467dc0d800e75d5df268?view=parallel',
-  commitUserName: 'crockitwood2',
-  commitUserUrl: 'http://minio.staging.saas.hand-china.com/iam-service/file_d0c99fab16c14c38ac023bca0110ac05_WX20180720-141353%402x.png',
-}, {
-  commitsId: 3,
-  commitContent: 'Update .gitlab-ci.yml',
-  commitDate: '2018-08-15 10:14:10',
-  commitUrl: 'http://git.staging.saas.hand-china.com/operation-pro0815/app0815/commit/c4cfa9d84e29984c3af3467dc0d800e75d5df268?view=parallel',
-  commitUserName: 'crockitwood2',
-  commitUserUrl: 'http://minio.staging.saas.hand-china.com/iam-service/file_d0c99fab16c14c38ac023bca0110ac05_WX20180720-141353%402x.png',
-}, {
-  commitsId: 4,
-  commitContent: 'Update .gitlab-ci.yml',
-  commitDate: '2018-08-15 10:14:10',
-  commitUrl: 'http://git.staging.saas.hand-china.com/operation-pro0815/app0815/commit/c4cfa9d84e29984c3af3467dc0d800e75d5df268?view=parallel',
-  commitUserName: 'crockitwood2',
-  commitUserUrl: 'http://minio.staging.saas.hand-china.com/iam-service/file_d0c99fab16c14c38ac023bca0110ac05_WX20180720-141353%402x.png',
-}, {
-  commitsId: 5,
-  commitContent: 'Update .gitlab-ci.yml',
-  commitDate: '2018-08-15 10:14:10',
-  commitUrl: 'http://git.staging.saas.hand-china.com/operation-pro0815/app0815/commit/c4cfa9d84e29984c3af3467dc0d800e75d5df268?view=parallel',
-  commitUserName: 'crockitwood2',
-  commitUserUrl: 'http://minio.staging.saas.hand-china.com/iam-service/file_d0c99fab16c14c38ac023bca0110ac05_WX20180720-141353%402x.png',
-}];
 const dataSource = {
-  total: {
-    commits: 1330,
-    keys: ['9/12', '9/13', '9/14', '9/15', '9/16', '9/17', '9/12'],
-    value: [820, 932, 901, 934, 1290, 1330, 1320],
-  },
-  personal: [
+  totalCommitsDate: [
+    '2018-09-09 12:56:04',
+    '2018-09-14 09:56:53',
+    '2018-09-15 09:57:18',
+    '2018-09-14 09:56:53',
+    '2018-09-15 09:57:18',
+    '2018-09-14 09:56:53',
+    '2018-09-15 09:57:18',
+    '2018-09-14 09:56:53',
+    '2018-09-15 09:57:18',
+    '2018-09-28 09:57:36',
+    '2018-09-10 17:35:18',
+    '2018-09-04 16:56:36',
+    '2018-09-15 09:57:18',
+    '2018-09-28 09:57:36',
+    '2018-09-10 17:35:18',
+    '2018-09-04 09:32:13',
+    '2018-09-15 09:57:18',
+    '2018-09-28 09:57:36',
+    '2018-09-10 17:35:18',
+    '2018-09-04 09:32:13',
+    '2018-09-04 16:56:36',
+    '2018-09-16 16:56:36',
+    '2018-09-26 09:57:06',
+    '2018-09-11 16:56:36',
+    '2018-09-26 09:57:06',
+    '2018-09-11 16:56:36',
+    '2018-09-26 09:57:06',
+  ],
+  commitFormUserDTOList: [
+    {
+      name: 'rua',
+      imgUrl: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+      commitDates: [
+        '2018-09-09 12:56:04',
+      ],
+    },
+    {
+      name: 'rua',
+      imgUrl: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+      commitDates: [
+        '2018-09-14 09:56:53',
+        '2018-09-15 09:57:18',
+        '2018-09-28 09:57:36',
+      ],
+    },
+    {
+      name: 'rua',
+      imgUrl: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+      commitDates: [
+        '2018-09-10 17:35:18',
+      ],
+    },
     {
       name: '邮件营销',
-      avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-      commits: 230,
-      keys: ['9/12', '9/13', '9/14', '9/15', '9/16', '9/17', '9/12'],
-      value: [120, 132, 101, 134, 90, 230, 210],
-    },
-    {
-      name: '联盟广告',
-      avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-      commits: 330,
-      keys: ['9/12', '9/13', '9/14', '9/15', '9/16', '9/17', '9/12'],
-      value: [220, 182, 191, 234, 290, 330, 310],
-    },
-    {
-      name: '视频广告',
-      avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-      commits: 330,
-      keys: ['9/12', '9/13', '9/14', '9/15', '9/16', '9/17', '9/12'],
-      value: [150, 232, 201, 154, 190, 330, 410],
-    },
-    {
-      name: '直接访问',
-      avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-      commits: 330,
-      keys: ['9/12', '9/13', '9/14', '9/15', '9/16', '9/17', '9/12'],
-      value: [320, 332, 301, 334, 390, 330, 320],
+      imgUrl: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+      commitDates: [
+        '2018-09-06 09:32:13',
+        '2018-09-11 16:56:36',
+        '2018-09-26 09:57:06',
+      ],
     },
   ],
 };
 
-const children = [];
-for (let i = 10; i < 36; i++) {
-  children.push(<Option key={i.toString(36) + i}>{i.toString(36) + i}</Option>);
-}
+const commitFormRecordDTOPage = {
+  totalPages: 3,
+  totalElements: 12,
+  numberOfElements: 5,
+  size: 5,
+  number: 0,
+  content: [
+    {
+      id: 1,
+      appId: 1,
+      userId: '昨夜小楼又东风',
+      commitSha: null,
+      commitContent: 'i.7435yi.7435yi.7435yi.7435yi.7435yi.7435yi.7435yi.7435yi.7435yi.7435yi.7435yi.7435y',
+      commitUserUrl: 'http://minio.staging.saas.hand-china.com/iam-service/file_d0c99fab16c14c38ac023bca0110ac05_WX20180720-141353%402x.png',
+      ref: 'http://git.staging.saas.hand-china.com/operation-pro0815/app0815/commit/c4cfa9d84e29984c3af3467dc0d800e75d5df268?view=parallel',
+      commitDate: '2018-09-10 09:57:36',
+    },
+    {
+      id: 2,
+      appId: 1,
+      userId: 346,
+      commitSha: null,
+      commitContent: 'aw3t',
+      commitUserUrl: 'http://minio.staging.saas.hand-china.com/iam-service/file_d0c99fab16c14c38ac023bca0110ac05_WX20180720-141353%402x.png',
+      ref: 'http://git.staging.saas.hand-china.com/operation-pro0815/app0815/commit/c4cfa9d84e29984c3af3467dc0d800e75d5df268?view=parallel',
+      commitDate: '2018-09-15 09:57:18',
+    },
+    {
+      id: 3,
+      appId: 1,
+      userId: 346,
+      commitSha: null,
+      commitContent: 'i.7435y',
+      commitUserUrl: 'http://minio.staging.saas.hand-china.com/iam-service/file_d0c99fab16c14c38ac023bca0110ac05_WX20180720-141353%402x.png',
+      ref: 'http://git.staging.saas.hand-china.com/operation-pro0815/app0815/commit/c4cfa9d84e29984c3af3467dc0d800e75d5df268?view=parallel',
+      commitDate: '2018-09-28 09:57:36',
+    },
+    {
+      id: 4,
+      appId: 1,
+      userId: 346,
+      commitSha: null,
+      commitContent: 'aw3t',
+      commitUserUrl: 'http://minio.staging.saas.hand-china.com/iam-service/file_d0c99fab16c14c38ac023bca0110ac05_WX20180720-141353%402x.png',
+      ref: 'http://git.staging.saas.hand-china.com/operation-pro0815/app0815/commit/c4cfa9d84e29984c3af3467dc0d800e75d5df268?view=parallel',
+      commitDate: '2018-09-15 09:57:18',
+    },
+    {
+      id: 5,
+      appId: 1,
+      userId: 346,
+      commitSha: null,
+      commitContent: 'i.7435y',
+      commitUserUrl: 'http://minio.staging.saas.hand-china.com/iam-service/file_d0c99fab16c14c38ac023bca0110ac05_WX20180720-141353%402x.png',
+      ref: 'http://git.staging.saas.hand-china.com/operation-pro0815/app0815/commit/c4cfa9d84e29984c3af3467dc0d800e75d5df268?view=parallel',
+      commitDate: '2018-09-28 09:57:36',
+    },
+  ],
+};
 
 @observer
 class Submission extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      recent: null,
+      // rec: null
     };
+  }
+
+  componentDidMount() {
+    this.loadData();
   }
 
   handleRefresh = () => {};
 
   /**
-   * 用户、应用选择
+   * 应用选择
    * @param e
    */
   handleSelect = (e) => {
     console.log(e.target.value);
   };
 
-  onDataChange = (data, dataString) => {
-    console.log(data, dataString);
-  }
+  loadData = () => {
+    const { ReportsStore } = this.props;
+    const { id: projectId } = AppState.currentMenuType;
+    ReportsStore.loadApps(projectId);
+  };
 
   render() {
-    const { intl: { formatMessage }, history } = this.props;
+    const { intl: { formatMessage }, history, ReportsStore } = this.props;
     const { id, name, type, organizationId } = AppState.currentMenuType;
-    const { recent } = this.state;
-    const { total, personal } = dataSource;
-    const personChart = personal.map(item => (<div key={item.name} className="c7n-report-submission-item">
+    const apps = ReportsStore.getApps;
+    const options = _.map(apps, item => (<Option key={item.id} value={item.id}>{item.name}</Option>));
+    const { total, user } = formatData(dataSource);
+    const commits = commitFormRecordDTOPage;
+    const personChart = user.map(item => (<div key={item.name} className="c7n-report-submission-item">
       <LineChart
         name={item.name}
         color="#ff9915"
@@ -149,22 +233,16 @@ class Submission extends Component {
           <Select
             className=" c7n-report-control-select"
             mode="multiple"
-            placeholder="Please select"
-            defaultValue={['a10', 'c12']}
+            placeholder={formatMessage({ id: 'report.app.noselect' })}
+            maxTagCount={5}
+            maxTagPlaceholder={formatMessage({ id: 'report.app.more' })}
             onChange={this.handleSelect}
+            optionFilterProp="children"
+            filter
           >
-            {children}
+            {options}
           </Select>
-          <div className="c7n-report-control-recently">
-            <Button type={recent === 1 ? 'primary' : null}>{formatMessage({ id: 'report.data.today' })}</Button>
-            <Button>{formatMessage({ id: 'report.data.seven' })}</Button>
-            <Button>{formatMessage({ id: 'report.data.thirty' })}</Button>
-            <RangePicker
-              className="c7n-report-control-datepick"
-              // getCalendarContainer={triggerNode => triggerNode.parentNode}
-              onChange={this.onDataChange}
-            />
-          </div>
+          {/* <TimePicker /> */}
         </div>
         <div className="c7n-report-submission clearfix">
           <div className="c7n-report-submission-overview">
@@ -175,7 +253,7 @@ class Submission extends Component {
               data={total}
             />
           </div>
-          <div className="c7n-report-submission-history"><CommitHistory dataSource={commitsData} /></div>
+          <div className="c7n-report-submission-history"><CommitHistory dataSource={commits} /></div>
         </div>
         <div className="c7n-report-submission-wrap">{personChart}</div>
       </Content>
