cff45335cdcf7142e1eea09d5153d9faf513a123
ale <ale0720@163.com>
2018-11-23 11:33:28 +0800

    [ADD]Template interface add status

--- devops/src/app/devops/components/StatusIcon/StatusIcon.js
+++ devops/src/app/devops/components/StatusIcon/StatusIcon.js
@@ -1,36 +1,51 @@
-import React from 'react';
-import { injectIntl } from 'react-intl';
-import PropTypes from 'prop-types';
-import { Tooltip, Progress, Icon } from 'choerodon-ui';
-import './StatusIcon.scss';
+import React from "react";
+import { injectIntl } from "react-intl";
+import PropTypes from "prop-types";
+import { Tooltip, Progress, Icon } from "choerodon-ui";
+import MouseOverWrapper from "../../components/MouseOverWrapper";
+import "./StatusIcon.scss";
 
 function StatusIcon(props) {
-  const { status, error, name, intl: { formatMessage } } = props;
+  const {
+    status,
+    error,
+    name,
+    intl: { formatMessage },
+  } = props;
   let statusDom = null;
   switch (status) {
-    case 'failed':
-      statusDom = (<Tooltip title={`${status} ${error ? `:${error}` : ''}`}>
-        <Icon type="error" className="c7n-status-failed" />
-      </Tooltip>);
+    case "failed":
+      const msg = error ? `: ${error}` : "";
+      statusDom = (
+        <Tooltip title={`failed ${msg}`}>
+          <Icon type="error" className="c7n-status-failed" />
+        </Tooltip>
+      );
       break;
-    case 'operating':
-      statusDom = (<Tooltip title={formatMessage({ id: `ist_${status}` })}>
-        <Progress type="loading" width={15} className="c7n-status-progress" />
-      </Tooltip>);
+    case "operating":
+      statusDom = (
+        <Tooltip title={formatMessage({ id: `ist_operating` })}>
+          <Progress type="loading" width={15} className="c7n-status-progress" />
+        </Tooltip>
+      );
       break;
     default:
       statusDom = null;
   }
-  return (<React.Fragment>
-    <span className="c7n-status-text">{name}</span>
-    {statusDom}
-  </React.Fragment>);
+  return (
+    <React.Fragment>
+      <MouseOverWrapper className="c7n-status-text" text={name} width={0.15}>
+        {name}
+      </MouseOverWrapper>
+      {statusDom}
+    </React.Fragment>
+  );
 }
 
 StatusIcon.propTypes = {
   status: PropTypes.string.isRequired,
-  error: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
+  error: PropTypes.string,
 };
 
 export default injectIntl(StatusIcon);
