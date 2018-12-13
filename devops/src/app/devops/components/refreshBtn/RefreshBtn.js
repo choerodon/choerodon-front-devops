bc8a6794e636c11c3c30605f9bb37ad9de6da84d
ale <ale0720@163.com>
2018-12-13 15:06:31 +0800

    [IMP]imp auto refresh

--- devops/src/app/devops/components/refreshBtn/RefreshBtn.js
+++ devops/src/app/devops/components/refreshBtn/RefreshBtn.js
@@ -1,15 +1,9 @@
 import React, { Component } from "react";
 import { injectIntl, FormattedMessage } from "react-intl";
 import PropTypes from "prop-types";
-import { Icon, Radio, Dropdown } from "choerodon-ui";
+import { Switch, Button } from "choerodon-ui";
 import DevopsStore from "../../stores/DevopsStore";
 import "./index.scss";
-
-const { Group: RadioGroup } = Radio;
-const { Button: DropdownButton } = Dropdown;
-
-const REFRESH_MANUAL = "manual";
-const REFRESH_AUTOMATIC = "auto";
 
 @injectIntl
 export default class RefreshBtn extends Component {
@@ -19,54 +13,30 @@ export default class RefreshBtn extends Component {
   };
 
   onChange = e => {
-    const {
-      onFresh,
-      name,
-      intl: { formatMessage },
-    } = this.props;
+    const { onFresh, name } = this.props;
+
     DevopsStore.clearTimer();
+    DevopsStore.setAutoFlag({ [name]: e });
+    DevopsStore.setSwitchValue(e);
 
-    if (e.target.value === REFRESH_AUTOMATIC) {
-      Choerodon.prompt(formatMessage({ id: "refresh.auto.open" }));
+    if (e) {
       DevopsStore.setTimer(onFresh);
-      DevopsStore.setAutoFlag({ [name]: true });
-    } else {
-      DevopsStore.setAutoFlag({ [name]: false });
-      Choerodon.prompt(formatMessage({ id: "refresh.auto.close" }));
     }
-
-    DevopsStore.setRadioValue(e.target.value);
   };
 
   render() {
     const { onFresh } = this.props;
 
-    const radioValue = DevopsStore.getRadioValue;
+    const value = DevopsStore.getSwitchValue;
 
-    const menu = (
-      <RadioGroup
-        className="c7ncd-refresh-radioGroup"
-        onChange={this.onChange}
-        value={radioValue}
-      >
-        <Radio className="c7ncd-refresh-radio" value={REFRESH_MANUAL}>
-          <FormattedMessage id="refresh.manual" />
-        </Radio>
-        <Radio className="c7ncd-refresh-radio" value={REFRESH_AUTOMATIC}>
-          <FormattedMessage id="refresh.auto" />
-        </Radio>
-      </RadioGroup>
-    );
-
-    return (
-      <DropdownButton
-        onClick={() => onFresh(true)}
-        className="c7ncd-refresh"
-        overlay={menu}
-      >
-        <Icon type="refresh" />
+    return [
+      <Button key="refresh" icon="refresh" onClick={() => onFresh(true)}>
         <FormattedMessage id="refresh" />
-      </DropdownButton>
-    );
+      </Button>,
+      <div key="label" className="c7ncd-refresh-label">
+        <FormattedMessage id="refresh.auto" />
+      </div>,
+      <Switch key="switch" onChange={this.onChange} checked={value} />,
+    ];
   }
 }
